af965ee5c01f0a2f51e8e9e80b3702280def36a6
Marcus Mellor <infinitymdm@gmail.com>
2025-04-30 11:06:20 -0500

    Transparent ghostty cursor, update email, add setup script

---
+++ solus/init_setup.sh
@@ -0,0 +1,6 @@
+eopkg.bin rmf gedit gnome-terminal libreoffice-common rhythmbox thunderbird
+eopkg.bin up
+eopkg.bin it -c system.devel
+eopkg.bin it -c desktop.font
+eopkg.bin it -c multimedia.codecs
+eopkg.bin it bat budgie-weathershow-applet dconf-editor element fwupd git github-cli glow go-task hugo inkscape iverilog just lightdm-settings nextpnr-ice40 ngspice numpy openssh-server virt-manager solbuild solbuild-config-unstable solbuild-config-local-unstable starship tcl tree typst verilator zotero
